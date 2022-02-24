e0daa9794b605873519889775c8ac15ec822b4d5
alexmatandos <amatand@clemson.edu>
2022-02-24 11:49:45 -0500

    Support Vector Machine

--- use_svm.py
+++ use_svm.py
@@ -1,7 +1,9 @@
 import kfold_template
 import pandas
 from matplotlib import pyplot
+from sklearn import svm
 
+#SVM: Support Vector Machine
 dataset = pandas.read_csv("dataset_svm_1.csv")
 
 target = dataset.iloc[:, 0].values
@@ -12,3 +14,10 @@ pyplot.savefig("scatterplot1.png")
 pyplot.close()
 
 #looking at the plot you can see that there's a overlap between yellow and purple points
+
+r2_scores, accuracy_scores, confusion_matrices = kfold_template.run_kfold(data, target, 4, svm.SVC(kernel = "linear"), 1, 1)
+
+print(r2_scores)
+print(accuracy_scores)
+
+#fix the confusion matrices
